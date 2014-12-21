0d570e497339d51c9237f2c175d9cbf1cc9a5372
rosasha <zzzenit@mail.ru>
2014-12-22 01:27:36 +0300

    cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
-## Write a short comment describing this function
+## INVERSE MATRIX (Return a matrix that is the inverse of 'x')
+
 makeCacheMatrix <- function(x = matrix()) {
-        mm <- NULL
-        set <- function(y) {
+        mm <- NULL #In makeCacheMatrix : object created , solve set to NULL
+        set <- function(y) { #Set function, new value being assigned to x
                 x <<- y
-                mm <<- NULL
+                mm <<- NULL #and hence setting solve as NULL
          }
-         get <- function() x
-         setsolve <- function(solve) mm <<- solve
-         getsolve <- function() mm
-         list(set = set, get = get,
+         get <- function() x #In Get object function, will return object
+         setsolve <- function(solve) mm <<- solve #In set mean function , cache solve
+         getsolve <- function() mm #In Get object function, will return object
+         list(set = set, get = get, #list of setters getters
               setsolve = setsolve,
               getsolve = getsolve)
 }
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cachesolve <- function(x, ...) { #In cachesolve : Object x calling get solve
         mm <- x$getsolve()
-        if(!is.null(mm))
-                message("getting cached data")
-        return(m)
-data <- x$get()
-mm <- solve(data, ...)
-x$setsolve(mm)
-print(mm)
-}
-x<- matrix(sample.int(100,size=9,replace=TRUE), nrow=3) 
-    
-        ## Return a matrix that is the inverse of 'x'
-x
+        if(!is.null(mm)){ #cached solve not NULL, returning cached solve
+        return(mm)
+        }
+data <- x$get() #find solve of data
+mm <- solve(data, ...) #call set solve
+x$setsolve(mm) #and return solve
 mm
+}
 
-
-
+#for checking:
+#matr1<-matrix(sample.int(100,size=9,replace=TRUE), nrow=3)
+#bigvec<- makeCacheMatrix(matr1) 
+#cachesolve(bigvec)   
+#solve(matr1)
+#matr1
 
